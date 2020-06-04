520147c42b1bdf8f4ce4df786bcb91e34717c58e
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-06-04 09:37:04 -0400

    Fixed bug

--- App.js
+++ App.js
@@ -4,10 +4,6 @@ import { Platform, StyleSheet, Text, View, Vibration } from 'react-native';
 import AppContainer from "./src/AppContainer/AppContainer";
 import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
-import ExpoToken from "./src/services/ExpoToken/ExpoToken";
-import { Notifications } from 'expo';
-import * as Permissions from 'expo-permissions';
-import Constants from 'expo-constants';
 
 const Stack = createStackNavigator();
 
