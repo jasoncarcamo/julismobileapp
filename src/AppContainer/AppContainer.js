6bbbf22e1c047895ac6e4417b0a8718357c6c716
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-06-04 09:22:36 -0400

    Back on track

--- src/AppContainer/AppContainer.js
+++ src/AppContainer/AppContainer.js
@@ -43,7 +43,7 @@ export default class AppContainer extends React.Component{
 
                         });
                 };
-            })
+            });
     }
 
     registerForPushNotificationsAsync = async () => {
