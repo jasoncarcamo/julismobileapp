a31359370235e4a7ba48e1cfc138b8445e49f15c
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-06-03 22:33:47 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,27 +1,29 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import AppContainer from "./src/AppContainer/AppContainer";
-import {NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator} from "@react-navigation/stack";
-
-const Stack = createStackNavigator();
+import {StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default class App extends React.Component{
 
     render(){
         return (
-            <NavigationContainer
-                >
-                <Stack.Navigator>
-                    <Stack.Screen
-                        name="App"
-                        component={AppContainer}
-                        options={{
-                            headerShown: false
-                        }}></Stack.Screen>
-                </Stack.Navigator>
-            </NavigationContainer>
+            <View
+                style={DemoStyle.container}>
+                <Text
+                    style={DemoStyle.text}>Working</Text>
+            </View>
         )
     }
 };
+
+const DemoStyle = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: "100%"
+    },
+    text: {
+        position: "relative",
+        top: "50%",
+        fontSize: 20,
+        textAlign: "center"
+    }
+})
